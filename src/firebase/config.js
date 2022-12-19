953664f1154d5f0ee1386004ddb7c210c6e05431
Estevam Morais <33493699+estevamorais@users.noreply.github.com>
2022-12-19 14:58:44 -0300

    Fix Firebase import

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firebase";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
