042acb5de06b43bcc3de603354c521c1bb722119
Estevam Morais <33493699+estevamorais@users.noreply.github.com>
2022-12-20 18:10:46 -0300

    Create about static page

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -1,7 +1,22 @@
 import styles from "./index.module.css";
 
+import { Link } from "react-router-dom";
+
 const About = () => {
-  return <div>About</div>;
+  return (
+    <div className={styles.about}>
+      <h1>
+        About React <span>Blog</span>
+      </h1>
+      <p>
+        This project consists of a blog made with React on the front-end and
+        Firebase on the back-end.
+      </p>
+      <Link to="/posts/create" className="btn">
+        New Post
+      </Link>
+    </div>
+  );
 };
 
 export default About;
